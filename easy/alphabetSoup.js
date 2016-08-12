554b109bbb054ef466baf4ca500fbc04c054dd55
Joanne <jeffiefeifei@gmail.com>
2016-08-11 20:31:46 -0700

    Algorithms solved (FYI: sortNumber() method provided

---
+++ easy/alphabetSoup.js
@@ -0,0 +1,19 @@
+// Using the JavaScript language, have the function AlphabetSoup(str) take the str
+// string parameter being passed and return the string with the letters in alphabetical
+// order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not
+// be included in the string.
+
+function AlphabetSoup(str) {
+  var arr = str.split("");
+  return arr.sort().join("");
+}
+
+
+//FYI: sort() method only works for Alphabet sort. If you would like to sort
+//number, such as [1,23,4,89], you need to write your own sortNumber().
+function sortNumber(arr) {
+  arr = arr.sort(function(a, b) {
+    return a - b;
+  })
+  return arr;
+}
