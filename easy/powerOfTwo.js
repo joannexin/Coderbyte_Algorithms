e4050226d928bfb68e69e049e59865a7b1827d50
Joanne <jeffiefeifei@gmail.com>
2016-08-25 22:23:52 -0700

    Algorithms solved

---
+++ easy/powerOfTwo.js
@@ -0,0 +1,13 @@
+// Using the JavaScript language, have the function PowersofTwo(num) take the num parameter being passed which will
+// be an integer and return the string true if it's a power of two. If it's not return the string false. For example
+// if the input is 16 then your program should return the string true but if the input is 22 then the output should
+// be the string false.
+
+function PowersofTwo(num) {
+  for (var i = 1; i <= num; i++) {
+    if (Math.pow(2, i) === num) {
+      return true;
+    }
+  }
+  return false;
+}
